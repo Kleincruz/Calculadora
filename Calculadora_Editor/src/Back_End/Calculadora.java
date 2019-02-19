26bf497d279a5b91299db214731268a6de86ab65
Kleincruz <32440158+Kleincruz@users.noreply.github.com>
2019-02-19 18:47:00 -0300

    Versão Básica com GUI +|-|*|/
    Primeira versão implementar segurança, inserir comentários, implementar Parenteses com pilha e inserir editor de texto

---
+++ Calculadora_Editor/src/Back_End/Calculadora.java
@@ -0,0 +1,29 @@
+package Back_End;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KleinCruz
+ */
+public class Calculadora {
+    
+    
+    public int soma (int a, int b){
+        return a+b;
+    }
+    public int subt (int a, int b){
+        return a-b;
+    }
+    public int mult (int a, int b){
+        return a*b;
+    }
+    public int divi (int a, int b){
+        return a/b;
+    }
+}
+
